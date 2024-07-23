95a0558f26755e1e2d1b41608ed0be5e7224dc37
varsha-arjava <varsha.arjava@gmail.com>
2024-07-23 16:46:51 +0530

    upload react Todo list

---
+++ React/starting-project/src/components/header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return (
+        <header>
+            <div style={{ textAlign:'center' }}>
+                    <img src="src/assets/image/todoLogo.png" alt="react-core-concept" width={200} />
+            </div>
+        </header>
+    )
+}
